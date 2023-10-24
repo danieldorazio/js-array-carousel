bb667456d2ccb2581efc57f4f4b96c3a2db19f7c
danieldorazio <danieldorazio.97@gmail.com>
2023-10-24 17:46:19 +0200

    add: LOGIC-JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 //DATI 
 const img = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
-
 //INSERIMENTO NEL DOM
 const itemsElem = document.querySelector(".items");
 
-let imgTag;
+let imgTag = ""; //da definire senno da undefine
 
 for (i = 0; i < img.length; i++) {
     const curImg = img[i];
@@ -15,8 +14,32 @@ for (i = 0; i < img.length; i++) {
     </div>`;
 }
 
-itemsElem.innerHTML = imgTag;
+itemsElem.innerHTML += imgTag;
 
 //CLASS ACTIVE
 const itemsElems = document.querySelectorAll(".item");
-console.log(itemsElems);
+
+
+let indiceImg = 0;
+itemsElems[indiceImg].classList.add("active");
+
+document.querySelector(".prev").addEventListener ("click", function(event){
+    event.preventDefault
+    if(indiceImg > 0) {
+        itemsElems[indiceImg].classList.remove ("active");
+        indiceImg--;
+        itemsElems[indiceImg].classList.add ("active")
+    }   
+})
+
+document.querySelector(".next").addEventListener ("click", function(event){
+    event.preventDefault
+    if(indiceImg < itemsElems.length -1) {
+        itemsElems[indiceImg].classList.remove ("active");
+        indiceImg++;
+        itemsElems[indiceImg].classList.add ("active")
+    }
+    
+})
+
+
