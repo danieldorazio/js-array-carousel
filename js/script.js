b2d641374cb84a3e03f1c2fade709e7eeed11728
danieldorazio <danieldorazio.97@gmail.com>
2023-11-01 17:45:28 +0100

    add: autoScrolling with mouseover

--- js/script.js
+++ js/script.js
@@ -1,45 +1,85 @@
-//DATI 
 const img = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
-
-//INSERIMENTO NEL DOM
 const itemsElem = document.querySelector(".items");
 
-let imgTag = ""; //da definire senno da undefine
+let imgTag = "";
 
 for (i = 0; i < img.length; i++) {
     const curImg = img[i];
     imgTag += 
-    `<div class="item">
+     `<div class="item">
         <img src="${curImg}" alt="">
     </div>`;
 }
 
 itemsElem.innerHTML += imgTag;
 
-//CLASS ACTIVE
-const itemsElems = document.querySelectorAll(".item");
+//trovare elemento "prev" "next" nella pagina questa ricerca va fatta sotto itemsElem.innerHTML += imgTag; perche senno non funzionando andado ad cancellare gli elementi prev e next
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
 
+
+const photo = document.querySelectorAll(".item");
 
 let indiceImg = 0;
-itemsElems[indiceImg].classList.add("active");
+photo[indiceImg].classList.add("active");
 
-document.querySelector(".prev").addEventListener ("click", function(event){
-    event.preventDefault
-    if(indiceImg > 0) {
-        itemsElems[indiceImg].classList.remove ("active");
-        indiceImg--;
-        itemsElems[indiceImg].classList.add ("active")
-    }   
+
+nextButton.addEventListener ("click", function(){
+
+    photo[indiceImg].classList.remove ("active");
+
+    if(indiceImg < photo.length -1) {
+        indiceImg++; 
+
+    } else {
+        indiceImg = 0;
+    }
+
+    photo[indiceImg].classList.add ("active");
 })
 
-document.querySelector(".next").addEventListener ("click", function(event){
-    event.preventDefault
-    if(indiceImg < itemsElems.length -1) {
-        itemsElems[indiceImg].classList.remove ("active");
-        indiceImg++;
-        itemsElems[indiceImg].classList.add ("active")
+
+
+
+
+prevButton.addEventListener ("click", function(){
+
+    photo[indiceImg].classList.remove ("active");
+    if(indiceImg > 0) {
+        indiceImg--;
+
+    } else {
+        indiceImg = photo.length -1;
+        
     }
     
+    photo[indiceImg].classList.add ("active");
 })
+
+
+
+let autoScrolling;
+
+itemsElem.addEventListener("mouseout", function () {
+    autoScrolling = setInterval (function () {
+        photo[indiceImg].classList.remove ("active");
+        if(indiceImg < photo.length -1) {
+            indiceImg++; 
+    
+        } else {
+            indiceImg = 0;
+        }
+    
+        photo[indiceImg].classList.add ("active");  
+    
+    
+    }, 1000);
+})
+
+
+itemsElem.addEventListener("mouseover", function () {
+    clearInterval(autoScrolling);  
+})
+
 
 
